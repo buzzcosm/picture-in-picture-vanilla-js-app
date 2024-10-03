13eced5d9d5b9875788b0b493219800df2c69751
Evgueni Kounik <kounik.ch@gmail.com>
2024-10-03 18:58:17 +0200

    restruct event logic

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const videoElement = document.getElementById('video');
 const pipActionBtn = document.getElementById('pip-action');
+const buttonContainer = document.querySelector('.button-container');
 const button = document.getElementById('button');
 
 // Prompt to select media stream, pass to video element, then play
@@ -10,20 +11,25 @@ async function selectMediaStream() {
     videoElement.onloadedmetadata = () => {
       videoElement.play();
     }
+    buttonContainer.hidden = false;
   } catch (error) {
     console.error('whoops, error here:', error);
   }
 }
 
-pipActionBtn.addEventListener('click', async () => {
-  selectMediaStream();
-})
-
-button.addEventListener('click', async () => {
+async function requestPictureInPicture() {
   // Disable button
   button.disabled = true;
   // Start picture in picture
   await videoElement.requestPictureInPicture();
   // Reset button
   button.disabled = false;
-})
+}
+
+async function leavePictureInPicture() {
+  console.log('leave picture-in-picture event');
+}
+
+pipActionBtn.addEventListener('click', selectMediaStream)
+button.addEventListener('click', requestPictureInPicture)
+videoElement.addEventListener('leavepictureinpicture', leavePictureInPicture);
